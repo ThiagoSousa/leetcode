6690bfe9ce982204b29a7e7dea2c7d96d296bf01
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-03-20 15:38:55 +0800

    Update and rename reverse-integer.py to reverse-integer-longint.py

--- 7-reverse-integer/reverse-integer-longint.py
+++ 7-reverse-integer/reverse-integer-longint.py
@@ -33,18 +33,5 @@ class Solution:
 
                 return int(str_x)
         return int(str_x)
-
-        # is_negative = True if x<0 else False
-        # number = [i for i in str(x).replace("-", "")]
-        # for i in range(int(len(number)/2)):
-        #     aux = number[i]
-        #     number[i] = number[len(number)-i-1]
-        #     number[len(number)-i-1] = aux
-        # number = int("".join(number))
-        # if is_negative:
-        #     number = number*-1
-        # if number>2**31-1 or number<-2**31-1:
-        #     return 0
-        # return number
             
         
