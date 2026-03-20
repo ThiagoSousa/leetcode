fbc5c1e083fb221f80af12101ea53ebda942e403
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-03-20 15:54:35 +0800

    Time: 3 ms (88.85%) | Memory: 19.1 MB (94.87%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_x = str(x)
+        for i in range(int(len(str_x)/2)):
+            if str_x[i] != str_x[i*-1-1]:
+                return False
+        return True
+
+        
