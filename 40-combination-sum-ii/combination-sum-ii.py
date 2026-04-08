c58302b9f713776d237dcee87bea7bb7f490fcb2
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-08 13:13:51 +0800

    Time: 15 ms (58.28%) | Memory: 19.4 MB (79.85%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        solutions = []
+        candidates.sort()
+
+        def backsearch(candidates, target, partial):
+
+            if target < 0:
+                return
+
+            if target == 0:
+                if partial not in solutions:
+                    solutions.append(partial)
+                return
+
+            for i in range(len(candidates)):
+                if i>0 and candidates[i]==candidates[i-1]:
+                    continue
+                if candidates[i]<=target:
+                    backsearch(candidates[i+1:], target-candidates[i], partial+[candidates[i]])
+                
+
+        backsearch(candidates, target, [])
+
+        return solutions
