90405f0e50bc7301fa73de064cf96f09e6d851ef
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-08 14:29:12 +0800

    Time: 1290 ms (9.47%) | Memory: 19.4 MB (39.39%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+
+        for i in range(1,x+1):
+            if i**2 > x:
+                return i-1
