3e9d3a7ad0cbd76a14efba5b30d4b2d3970d1f49
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-08 14:40:29 +0800

    Time: 23 ms (20.22%) | Memory: 19.2 MB (71.10%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -5,6 +5,22 @@ class Solution:
         if x == 1:
             return 1
 
-        for i in range(1,x+1):
-            if i*i > x:
-                return i-1
+        # for i in range(1,x+1):
+        #     if i*i > x:
+        #         return i-1
+        left = 1
+        right = x-1
+        result = -1
+        while left<=right:
+            middle = left+int((right-left)/2)
+            print(left, right, middle)
+            middle_sq = middle*middle
+            if middle_sq == x:
+                return middle
+            elif middle_sq > x:
+                right = middle - 1
+            else:
+                result = middle
+                left = middle + 1
+        return result
+            
