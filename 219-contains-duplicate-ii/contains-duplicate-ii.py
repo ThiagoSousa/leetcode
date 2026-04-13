42a752715af499e8c9a575a108d2fa37f33fdf75
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-13 20:52:58 +0800

    Time: 68 ms (8.15%) | Memory: 46 MB (5.11%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -4,23 +4,9 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] in d: 
                 for j in d[nums[i]]:
-                    if i!=j and abs(i-j)<=k:
+                    if abs(i-j)<=k:
                         return True
             else:
                 d[nums[i]] = []
             d[nums[i]].append(i)
         return False
-
-        # d = dict()
-        # for i in range(len(nums)):
-        #     if nums[i] not in d:
-        #         d[nums[i]] = []
-        #     d[nums[i]].append(i)            
-        
-        # for number in d:
-        #     for i in range(len(d[number])):
-        #         for j in range(i+1, len(d[number])):
-        #             if abs(d[number][i]-d[number][j])<=k:
-        #                 return True
-        # return False
-
