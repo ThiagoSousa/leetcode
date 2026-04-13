db7cce25b096ec0a5307aa840d9d9852fe7f2a55
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-13 20:49:18 +0800

    Time: 287 ms (5.02%) | Memory: 46.9 MB (5.11%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,12 +1,29 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # d = dict()
+        # for i in range(len(nums)):
+        #     if nums[i] in d: 
+        #         for j in d[nums[i]]:
+        #             if i!=j and abs(i-j)<=k:
+        #                 return True
+        #     else:
+        #         d[nums[i]] = []
+        #     d[nums[i]].append(i)
+        # return False
+
         d = dict()
         for i in range(len(nums)):
-            if nums[i] in d: 
-                for j in d[nums[i]]:
-                    if i!=j and abs(i-j)<=k:
-                        return True
-            else:
+            if nums[i] not in d:
                 d[nums[i]] = []
-            d[nums[i]].append(i)
+            d[nums[i]].append(i)            
+        
+        print(d)
+
+        for number in d:
+            print(number)
+            for i in range(len(d[number])):
+                for j in range(i+1, len(d[number])):
+                    if abs(d[number][i]-d[number][j])<=k:
+                        return True
         return False
+
