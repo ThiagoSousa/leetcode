34eac733e995609e6bc42a1d844af41f46946a1f
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-15 15:29:07 +0800

    Time: 378 ms (36.46%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select email from (select email, count(*) as c from Person group by email) as count_email where c>1
+select email from Person group by email having count(*)>1
