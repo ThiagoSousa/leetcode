26d2692665df89b5a9206f1c3a1ea08cd94665ac
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-16 23:49:19 +0800

    Time: 2 ms (61.62%) | Memory: 20.5 MB (49.92%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         max_n = max(nums)
-        for i in range(max_n+1):
-            if i not in nums:
+        aux_vector = [0 for i in range(max_n+1)]
+        for i in nums:
+            aux_vector[i] = 1
+        for i in range(len(aux_vector)):
+            if aux_vector[i] == 0:
                 return i
         return max_n+1
