d44d2004b145183c67f016b19238cac42c9499ca
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-17 20:28:56 +0800

    Time: 23 ms (52.24%) | Memory: 19.6 MB (77.96%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d = {}
+        for c in magazine:
+            d[c] = d.get(c, 0)+1
+        for c in ransomNote:
+            if c not in d:
+                return False
+            elif d[c] <=0:
+                return False
+            else:
+                d[c] -= 1
+        return True
