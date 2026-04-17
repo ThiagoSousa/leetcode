6d2b9e73e1e8f6d2ee33691269c5558bb81e9cd9
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-17 16:43:23 +0800

    Update power-of-four.py

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -1,5 +1,10 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
+        # if n<=0:
+        #     return False
+        # while n%4==0:
+        #     n = int(n/4)
+        # return n==1
         if n<=0:
             return False
         return (n & (n-1)) == 0 and n%3==1
