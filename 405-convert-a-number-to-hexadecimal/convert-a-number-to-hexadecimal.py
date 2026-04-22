f2cfe3e8f0d7352dbb4d025fcc534bdd7465cf23
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-22 23:43:36 +0800

    Time: 1 ms (10.13%) | Memory: 19.3 MB (31.86%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
@@ -0,0 +1,17 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        hex_map = {10: "a", 11:"b", 12:"c", 13:"d", 14:"e", 15:"f"}
+        if num == 0:
+            return "0"
+        if num == 1:
+            return "1"
+        if num<0:
+            num += 4294967296
+
+        hex_num = []
+        while num>=1:
+            hex_num.append(int(num%16))
+            num /= 16
+
+        hex_num = [str(i) if i<10 else hex_map[i] for i in hex_num[::-1]]
+        return "".join(hex_num)
