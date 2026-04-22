035d69329258709a81c050cff344a027c17a3c19
Thiago de Sousa Silveira <thiagosousasilveira@gmail.com>
2026-04-22 21:24:44 +0800

    Time: 15 ms (11.01%) | Memory: 23.5 MB (23.30%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        for i in range(int(len(s)/2)):
+            aux = s[i]
+            s[i] = s[len(s)-i-1]
+            s[len(s)-i-1] = aux
+        print(s)
+        
+            
